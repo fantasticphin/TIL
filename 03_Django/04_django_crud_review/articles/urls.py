d8250f6bf171bc3c4a84e1d1d9e0c8553a3df6ba
Phin <phineasawesome@gmail.com>
2019-10-04 11:27:23 +0900

    Django.10.04.Review

--- 03_Django/04_django_crud_review/articles/urls.py
+++ 03_Django/04_django_crud_review/articles/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.index),
     path('new/', views.new),
     path('create/', views.create),
+    path('<int:pk>/', views.detail), # article / 숫자를 넣으면 views 의 deatil 로 넘어가게 됌 // url 경로로 들어왔을 떄 변수 이름을 생성하면, views 에서도 동일한 이름으로 전달받음
+    path('<int:pk>/delete/', views.delete),
+    path('<int:pk>/edit/', views.edit),
+    path('<int:pk>/update/', views.update),
 ]
--- 03_Django/04_django_crud_review/articles/views.py
+++ 03_Django/04_django_crud_review/articles/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render, redirect
+from django.core.exceptions import ValidationError
 from .models import Article
+
+def create(request):
+    try:
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        article = Article(title=title, content=content)
+        article.full.clean() #유효성 검사
+    except ValidationError:
+        raise ValidationError('Your Error Message')
+    else:
+        article.save()
+        return redirect(f'/articles/{article.pk}/')
 
 def index(request):
     # articles = Article.objects.all()[::-1]
@@ -11,7 +24,7 @@ def index(request):
 
 def new(request): #사용자가 글을 입력할 수 있도록 도와주는 함수
     return render(request, 'articles/new.html')
-
+'''
 def create(request): #new 와 달리 사용자가 상호작용
     title = request.POST.get('title') #quarry dictionary 형태임
     # print(title)
@@ -24,13 +37,42 @@ def create(request): #new 와 달리 사용자가 상호작용
     # article.save()
 
     # #2 두번째 방법
-    # article = Article(title =title, content=content)
-    # article.save()
+    article = Article(title =title, content=content)
+    article.save()
 
     #3 세번쨰 방법, save method 불필요
-    Article.objects.create(title=title, content=content)
+    # Article.objects.create(title=title, content=content)
     
+    return redirect(f'/articles/{article.pk}/') #원래는 return render 이지만 redirect 함수를 적용한 이유는 '되돌아 가거나' get or post 요쳥에 따라 redirect 변경됌
+    '''
+def detail(request, pk): #pk 는 특정 글 하나를 잡아서 보여줘야 해서 pk 라는 인식표가 필요함
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article,
+    }
+
+    return render(request, 'articles/detail.html', context)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+
     return redirect('/articles/')
 
-def detail(request, pk):
-    pass
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article':article,
+    }
+
+    return render (request, 'articles/edit.html', context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk) #query set 를 챙겨온다, pk 의 글이 담겨있음
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    article.title = title
+    article.content = content
+    article.save()
+
+    return redirect(f'/articles/')
